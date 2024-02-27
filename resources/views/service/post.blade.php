c2296bdad3cc00b73fb293109cdb1156a41b9f55
sornkea <kea3rd@gmail.com>
2024-02-27 15:47:45 +0700

    add some landingpage

--- resources/views/service/post.blade.php
+++ resources/views/service/post.blade.php
@@ -1,19 +1,10 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends("layouts.app")
 
-<ul>
-    @foreach($posts as $p)
-        <li>{{$p['title']}} - {{$p['content']}}</li>
-    @endforeach
-</ul>
+@section('content')
+    <ul>
+        @foreach($posts as $p)
+            <li>{{$p['title']}} - {{$p['content']}}</li>
+        @endforeach
+    </ul>
 
-</body>
-</html>
+@endsection
